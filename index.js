ab358712503752d2945e556b1a0d24357c3f3e60
Chetan <chetankatwa12@gmail.com>
2021-12-23 17:32:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,12 @@
 const PORT = process.env.PORT || 3000;
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const _ = require("lodash");
 
 const app = express();
 
 
 
 app.get("/", function(req, res){
-//res.send("Hi Hello World!");
-  // const day = date.getDate();
-
-  // npm install crypto
-var crypto = require('crypto');
-// npm install request
-var request = require('request');
-
-// Set these in your ENVironment, or enter them here with the actual string
-var apiKey = '';
-var apiSecret = '';
-
-
-//get unix time in seconds
-var timestamp = Math.floor(Date.now() / 1000);
-
-// set the parameter for the request message
-var req = {
-    method: 'GET',
-    path: '/v2/exchange-rates?currency=USD',
-    body: ''
-};
-
-var message = timestamp + req.method + req.path + req.body;
-console.log(message);
-
-//create a hexedecimal encoded SHA256 signature of the message
-var signature = crypto.createHmac("sha256", apiSecret).update(message).digest("hex");
-
-//create the request options object
-var options = {
-    baseUrl: 'https://api.coinbase.com/',
-    url: req.path,
-    method: req.method,
-    headers: {
-        'CB-ACCESS-SIGN': signature,
-        'CB-ACCESS-TIMESTAMP': timestamp,
-        'CB-ACCESS-KEY': apiKey,
-        'CB-VERSION': '2015-07-22'
-    }
-};
-
-request(options,function(err, response){
-    if (err) console.log(err);
-    console.log(response.body);
-});
-Se
-
-   
+  res.send("Hi Hello World!"); 
 });
 
 
